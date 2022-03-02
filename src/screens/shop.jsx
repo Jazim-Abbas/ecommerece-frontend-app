2d791fe7f83558f32fc1339b7ee078307d6e811b
Jazim Abbas <jazimabbas87@gmail.com>
2022-03-02 21:30:17 +0500

    shop: wrap anchor tag to each item and remove view button

--- src/screens/shop.jsx
+++ src/screens/shop.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Card, Button, ListGroup, ListGroupItem, Modal } from "react-bootstrap";
 import BaseLayout from "../layouts/base";
+import { Link } from "react-router-dom";
 
 export default function ShopScreen() {
   const [showItemModal, setShowItemModal] = useState(false);
@@ -116,26 +117,25 @@ function ShopItem() {
   };
 
   return (
-    <Card>
-      <Card.Img variant="top" src={imageURL} />
-      <Card.Body>
-        <Card.Title>Item Name</Card.Title>
-        <Card.Text>Price: $32</Card.Text>
-        <Card.Text>Category</Card.Text>
-        <Card.Text>Total Sales: $20</Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroupItem>
-          <p>Description</p>
-        </ListGroupItem>
-        <ListGroupItem>
-          <Button size="sm" variant="primary" onClick={handleViewItem}>
-            View
-          </Button>
-          <i class="fa fa-heart float-left mx-4 pointer" aria-hidden="true" />
-          <i>Available</i>
-        </ListGroupItem>
-      </ListGroup>
-    </Card>
+    <Link className="view-link" to="/item/1">
+      <Card>
+        <Card.Img variant="top" src={imageURL} />
+        <Card.Body>
+          <Card.Title>Item Name</Card.Title>
+          <Card.Text>Price: $32</Card.Text>
+          <Card.Text>Category</Card.Text>
+          <Card.Text>Total Sales: $20</Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>
+            <p>Description</p>
+          </ListGroupItem>
+          <ListGroupItem>
+            <i class="fa fa-heart float-left pointer" aria-hidden="true" />
+            <i className="mx-3">Available</i>
+          </ListGroupItem>
+        </ListGroup>
+      </Card>
+    </Link>
   );
 }
