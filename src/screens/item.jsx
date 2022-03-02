eb4766f1fb3a3b29090c41afe8b80003379e64c2
Jazim Abbas <jazimabbas87@gmail.com>
2022-03-02 10:50:23 +0500

    single-item: added image carousel effect

--- src/screens/item.jsx
+++ src/screens/item.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, ListGroup } from "react-bootstrap";
+import { Button, ListGroup, Carousel } from "react-bootstrap";
 import BaseLayout from "../layouts/base";
 
 const imageURL =
@@ -10,7 +10,7 @@ export default function ItemScreen() {
     <BaseLayout>
       <div className="row">
         <div className="col-md-6">
-          <img src={imageURL} alt="Single Item Image" />
+          <ItemImagesCarousel />
         </div>
         <div className="col-md-6">
           <ListGroup>
@@ -57,16 +57,34 @@ export default function ItemScreen() {
             </ListGroup.Item>
             <CartOption />
             <ListGroup.Item>
-             <div className="d-grid">
-             <Button variant="light" size="lg">
-                Add to Cart
-              </Button>
-             </div>
+              <div className="d-grid">
+                <Button variant="light" size="lg">
+                  Add to Cart
+                </Button>
+              </div>
             </ListGroup.Item>
           </ListGroup>
         </div>
       </div>
     </BaseLayout>
+  );
+}
+
+function ItemImagesCarousel() {
+  const image1 =
+    "https://media.istockphoto.com/photos/bakery-chef-prepare-pizza-picture-id1291299956?b=1&k=20&m=1291299956&s=170667a&w=0&h=Ys_FLtdY0Uzc7yTQl6JzvCHTQ3eRAuqNNU4x8EX1FB8=";
+  const image2 =
+    "https://media.istockphoto.com/photos/homemade-indian-chicken-tikka-masala-pizza-picture-id1340589333?b=1&k=20&m=1340589333&s=170667a&w=0&h=rQaaNq-2klpZXcZ26amIfELKxq78-9S79FycY7Kzu28=";
+
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img src={image1} alt="Single Item Image" className="d-block w-100" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={image2} alt="Single Item Image" className="d-block w-100" />
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
