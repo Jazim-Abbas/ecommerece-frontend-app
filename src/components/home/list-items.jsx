613b3c8422a36eb1714e51186fffcb44820a37f9
Jazim Abbas <jazimabbas87@gmail.com>
2022-03-07 08:14:06 +0500

    render items on screen

--- src/components/home/list-items.jsx
+++ src/components/home/list-items.jsx
@@ -4,10 +4,11 @@ import { Card, Button, ListGroup, ListGroupItem } from "react-bootstrap";
 import * as itemApi from "../../apis/item";
 import useApi from "../../hooks/use-api";
 import AppLoading from "../loading";
+import { getImageURL } from "../../utils/app";
 
 export default function ListItems() {
   const items = [1, 2, 3, 4, 5];
-  const item = useApi(itemApi.getAllItems);
+  const item = useApi(itemApi.getAllItems, { keyExtractor: "items" });
 
   useEffect(() => {
     item.request();
@@ -15,30 +16,31 @@ export default function ListItems() {
 
   if (item.isLoading) return <AppLoading />;
 
-  return items.map((_, i) => (
-    <div className="col-md-3">
-      <Item key={i} />
-    </div>
-  ));
+  if (!item.data) return <></>;
+
+  if (item.data) {
+    return item.data.map((_item, i) => (
+      <div className="col-md-3">
+        <Item key={i} item={_item} />
+      </div>
+    ));
+  }
 }
 
-function Item() {
+function Item({ item }) {
   const imageURL =
     "https://images.unsplash.com/photo-1645917864901-1fa7731b9af6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60";
-
-  const history = useHistory();
-
-  const handleViewItem = () => {
-    history.push("/item/1");
-  };
 
   return (
     <Link className="view-link" to="item/1">
       <Card>
-        <Card.Img variant="top" src={imageURL} />
+        <Card.Img
+          variant="top"
+          src={item.featuredImage ? getImageURL(item.featuredImage) : imageURL}
+        />
         <Card.Body>
-          <Card.Title>Item Name: Pizza</Card.Title>
-          <Card.Text>Price: $32</Card.Text>
+          <Card.Title>Item Name: {item.name}</Card.Title>
+          <Card.Text>Price: ${item.price}</Card.Text>
           <Card.Text>
             <i class="fa fa-heart float-left pointer" aria-hidden="true" />
           </Card.Text>
---
+++ src/utils/app.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://localhost:8000/";
+
+export function getImageURL(imageURL) {
+  return BASE_URL + imageURL;
+}
