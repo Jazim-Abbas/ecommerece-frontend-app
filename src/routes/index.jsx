77b3f7ed0657bce6bea94939c405d770827fd7dc
Jazim Abbas <jazimabbas87@gmail.com>
2022-03-07 07:39:35 +0500

    added is auth middleware for protecting route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,13 @@
 import { Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import HomeScreen from "../screens/home";
+import PrivateRoute from "./private-route";
 import { PRIVATE_ROUTES, PUBLIC_ROUTES } from "./routes-list";
 
 export default function AppRoutes() {
   return (
     <Switch>
-      <Route exact path="/">
-        <HomeScreen />
-      </Route>
+      <PrivateRoute exact path="/" component={HomeScreen} />
 
       {PUBLIC_ROUTES.map((route) => (
         <Route key={route.path} path={route.path}>
@@ -17,9 +16,11 @@ export default function AppRoutes() {
       ))}
 
       {PRIVATE_ROUTES.map((route) => (
-        <Route key={route.path} path={route.path}>
-          <route.Component />
-        </Route>
+        <PrivateRoute
+          key={route.path}
+          path={route.path}
+          component={route.Component}
+        />
       ))}
     </Switch>
   );
