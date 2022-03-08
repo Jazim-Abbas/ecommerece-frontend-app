9a356de688da030e76ec400a3d4a353e72809195
Jazim Abbas <jazimabbas87@gmail.com>
2022-03-08 16:44:19 +0500

    item-category: create new category and add them to existing categories on the screen

--- src/apis/item-category.js
+++ src/apis/item-category.js
@@ -3,3 +3,7 @@ import api from ".";
 export function getAllCategories(shopId) {
   return api.get("/item-category/" + shopId);
 }
+
+export function createNewCategory(category) {
+  return api.post("/item-category", category);
+}
--- src/screens/item-category.jsx
+++ src/screens/item-category.jsx
@@ -3,10 +3,11 @@ import * as shopApi from "../apis/shop";
 import * as itemCategoryApi from "../apis/item-category";
 import useApi from "../hooks/use-api";
 import { useEffect, useState } from "react";
-import { AppLoading } from "../components";
+import { AppLoading, ServerError } from "../components";
 import { Link } from "react-router-dom";
 
 export default function ItemCategoryScreen() {
+  const [newAddedCateg, setNewAddedCateg] = useState();
   const shopDetailApi = useApi(shopApi.getShopDetail, {
     keyExtractor: "shop",
   });
@@ -14,6 +15,10 @@ export default function ItemCategoryScreen() {
   useEffect(() => {
     shopDetailApi.request();
   }, []);
+
+  const handleNewCategAdded = (category) => {
+    setNewAddedCateg(category);
+  };
 
   if (shopDetailApi.isLoading) return <AppLoading />;
 
@@ -30,12 +35,60 @@ export default function ItemCategoryScreen() {
 
   return (
     <BaseLayout>
-      <CategoryList shop={shopDetailApi.data} />
+      <CreateNewCategory
+        shop={shopDetailApi.data}
+        onNewCategAdded={handleNewCategAdded}
+      />
+      <hr />
+      <CategoryList shop={shopDetailApi.data} newCateg={newAddedCateg} />
     </BaseLayout>
   );
 }
 
-function CategoryList({ shop }) {
+function CreateNewCategory({ shop, onNewCategAdded }) {
+  const newCategoryApi = useApi(itemCategoryApi.createNewCategory, {
+    hasCatchError: true,
+  });
+  const [categName, setCateName] = useState("");
+
+  const handleAddCategory = async () => {
+    try {
+      const res = await newCategoryApi.request({
+        name: categName,
+        shopId: shop.id,
+      });
+      const newCateg = res.data.category;
+      onNewCategAdded(newCateg);
+      setCateName("");
+    } catch (_) {}
+  };
+
+  return (
+    <>
+      <div className="my-3">
+        <ServerError error={newCategoryApi.error} />
+      </div>
+      <div className="row">
+        <div className="col-md-5">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Item Category Name e.g. Entertainment ..."
+            value={categName}
+            onChange={(e) => setCateName(e.target.value)}
+          />
+        </div>
+        <div className="col-md-4">
+          <button className="btn btn-success" onClick={handleAddCategory}>
+            Add
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function CategoryList({ shop, newCateg }) {
   const [categories, setCategories] = useState([]);
   const categoryiesApi = useApi(itemCategoryApi.getAllCategories);
 
@@ -45,6 +98,13 @@ function CategoryList({ shop }) {
       setCategories(categories);
     });
   }, []);
+
+  useEffect(() => {
+    if (newCateg) {
+      const newCategs = [...categories, newCateg];
+      setCategories(newCategs);
+    }
+  }, [newCateg]);
 
   if (categoryiesApi.isLoading) return <AppLoading />;
 
