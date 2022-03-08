affc1ba1c8954af7bface40ab540c3f428af7693
Jazim Abbas <jazimabbas87@gmail.com>
2022-03-08 17:13:33 +0500

    shop: remove unused comments

--- src/screens/shop.jsx
+++ src/screens/shop.jsx
@@ -1,35 +1,14 @@
-import { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { Card, Button, ListGroup, ListGroupItem, Modal } from "react-bootstrap";
+import { useState } from "react";
 import BaseLayout from "../layouts/base";
-import { Link } from "react-router-dom";
 import UpdateShop from "../components/shop/update-shop";
 import ItemModal from "../components/shop/item-modal";
 import ShopItems from "../components/shop/shop-item-list";
-import * as itemCategoryApi from "../apis/item-category";
-import useApi from "../hooks/use-api";
-import { AppLoading } from "../components";
 
 export default function ShopScreen() {
   const [shop, setShop] = useState();
   const [showItemModal, setShowItemModal] = useState(false);
   const [newItem, setNewItem] = useState();
   const [searchVal, setSearchVal] = useState("");
-  const allCategories = useApi(itemCategoryApi.getAllCategories, {
-    keyExtractor: "categories",
-  });
-
-  // useEffect(() => {
-  //   if (shop) {
-  //     allCategories.request(shop.id);
-  //   }
-  // }, [shop]);
-
-  // if (allCategories.isLoading) return <AppLoading />;
-
-  // console.log("categories: ", allCategories.data);
-
-  // if (!allCategories.data) return <p>Loading</p>;
 
   const handleSearch = (e) => {
     setSearchVal(e.target.value);
@@ -73,7 +52,6 @@ export default function ShopScreen() {
               onHide={() => setShowItemModal(false)}
               onItemAdded={handleItemAdded}
               shop={shop}
-              // categories={allCategories.data}
             />
           </div>
         </div>
